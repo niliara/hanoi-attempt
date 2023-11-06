ff33b5a510cd2a87f208e03a05b3bf86a85067c3
niliara <niljo98@gmail.com>
2023-11-06 15:51:56 +0100

    better gameplay

--- Program.cs
+++ Program.cs
@@ -2,12 +2,13 @@
 {
 	public static class Globals
 	{
-		public static List<int> pole1 = new List<int>() {3, 2, 1};
+		public static List<int> pole1 = new List<int>();
 		public static List<int> pole2 = new List<int>();
 		public static List<int> pole3 = new List<int>();
 		public static List<int>[] poles = new List<int>[3];
 
-		public static int maxSpace = 16;
+		public static int maxSpace;
+		public static int disks;
 	}
 
 	public static void Main(string[] args)
@@ -15,12 +16,44 @@
 		bool playing = true;
 		string answer;
 
+		Console.WriteLine("\nWelcome to Tower of Hanoi!!!");
+
+		
+		while (true){
+			Console.Write("\nWhat how many disks do you want? ");
+			try
+			{
+				Globals.disks = int.Parse(Console.ReadLine());
+				break;
+			}
+			catch (Exception e)
+			{
+				Console.WriteLine(e.Message);
+			}
+		}
+
+		if (Globals.disks < 3)
+		{
+			Globals.disks = 3;
+		}
+		
+		if (Globals.disks > 20)
+		{
+			Globals.disks=20;
+		}
+
 		Globals.poles[0] = Globals.pole1;
 		Globals.poles[1] = Globals.pole2;
 		Globals.poles[2] = Globals.pole3;
 
+		for (int i=Globals.disks; i>0; i--)
+		{
+			Globals.poles[0].Add(i);
+		}
 
-		Console.WriteLine("\nWelcome to Tower of Hanoi!!! --press h for help\n");
+		Globals.maxSpace = 5 + Globals.disks;
+
+
 		printHanoi();
 
 		while (playing)
@@ -41,6 +74,7 @@
 					break;
 				case "h":
 					Console.WriteLine("\nAvailable actions:\n- mv <colNumber(0,1,2)> <colNumber>\n- see : check towers\n- h : help\n- q : quit");
+					Console.WriteLine("\nThe objective of this game is to move all the disks to the 3rd pole (pole 2).");
 					break;
 				default:
 					Console.WriteLine("\nnot a valid action!! type h to see available actions");
@@ -54,7 +88,7 @@
 	public static void printHanoi()
 	{
 		int width;
-		for (int i=5; i>0; i--)
+		for (int i=Globals.disks+2; i>0; i--)
 		{
 			for (int r=0; r<Globals.poles.Length; r++)
 			{
